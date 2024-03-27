ffc5384676c3bafc104b447af5af28f8443e5773
Yangzhiqin <yangzhiqin@neuronads.com>
2024-03-27 17:37:37 +0800

    up

--- math.go
+++ math.go
@@ -1,13 +1,17 @@
 package misc
 
-func Max[T comparable](a, b T) T {
+type Comparable interface {
+	~int8 | ~uint8 | ~int | ~uint | ~int16 | ~uint16 | ~int32 | ~uint32 | ~int64 | ~uint64 | ~float32 | ~float64 | ~string
+}
+
+func Max[T Comparable](a, b T) T {
 	if a > b {
 		return a
 	}
 	return b
 }
 
-func Min[T comparable](a, b T) T {
+func Min[T Comparable](a, b T) T {
 	if a > b {
 		return b
 	}
