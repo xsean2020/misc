486b3636f5152c64e8325a4b6af8562304f1cacb
Yangzhiqin <yangzhiqin@neuronads.com>
2024-03-27 17:27:37 +0800

    up

--- math.go
+++ math.go
@@ -1,26 +1,22 @@
 package misc
 
-type Number interface {
-	~int8 | ~uint8 | ~int | ~uint | ~int16 | ~uint16 | ~int32 | ~uint32 | ~int64 | ~uint64 | ~float32 | ~float64
-}
-
-func Max[T Number](a, b T) T {
+func Max[T comparable](a, b T) T {
 	if a > b {
 		return a
 	}
 	return b
 }
 
-func Min[T Number](a, b T) T {
+func Min[T comparable](a, b T) T {
 	if a > b {
 		return b
 	}
 	return a
 }
 
-func In[T comparable](slice []T, a T) bool {
+func In[T any](slice []T, equal func(T) bool) bool {
 	for i := range slice {
-		if slice[i] == a {
+		if equal(slice[i]) {
 			return true
 		}
 	}
